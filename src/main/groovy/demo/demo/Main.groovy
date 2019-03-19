e20c04424a35ae0e217f1e8df6bbc76ed8e6b898
Thomas May <cogman10@hotmail.com>
2019-03-18 21:58:08 -0600

    counting

--- src/main/groovy/demo/demo/Main.groovy
+++ src/main/groovy/demo/demo/Main.groovy
@@ -5,35 +5,23 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class Main {
     static void main(String[] args) {
-        (1..6)
+        (1..7)
                 .collect { (int) Math.pow(10.0d, (double) it) }
-                .forEach(Main.&runDedup)
+                .forEach(Main.&runGrouping)
     }
 
-    static runDedup(int numElements) {
+    static runGrouping(int numElements) {
         List<Entity> e = []
         (1..numElements).collect(e, EntityGenerator.&generate)
         def start = System.currentTimeMillis()
-        def result = deduplicate(e)
+        def result = groupBy(e)
         def end = System.currentTimeMillis()
-        println("Cut down " + numElements + " to " + result.size() + " in " + (end - start) + "ms")
+        println("In " + numElements + " found " + result + " who had fish pet, favorite number 3, favorite color red.  Took " + (end - start) + "ms" )
     }
 
-    static List<Entity> deduplicate(List<Entity> entities) {
-        List<Entity> nonDuplicates = []
-        nonDuplicates.addAll(entities)
-        for (def i = nonDuplicates.listIterator(); i.hasNext();  ) {
-            def entity = i.next()
-            for (int j = i.nextIndex(); j < nonDuplicates.size(); ++j)
-                if (matches(nonDuplicates[j], entity)) {
-                    i.remove()
-                    break
-                }
-        }
-        return nonDuplicates
-    }
-
-    static boolean matches(Entity a, Entity b) {
-        return a == b
+    static Number groupBy(List<Entity> entities) {
+        entities.findAll { (3 == it.favoriteNumber) }
+                .findAll { it.pet == Pet.fish }
+                .count { it.favoriteColor == "red" }
     }
 }
